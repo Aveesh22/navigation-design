fb26210d8951875590827e55bfb5bcf69ae2665f
patdziedzic <danieldziedzic93@gmail.com>
2023-11-26 19:54:34 -0500

    Generated Javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.order.navigationdesign","l":"com.order.navigationdesign"}];updateSearchResults();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.order.navigationdesign {
     requires javafx.controls;
     requires javafx.fxml;
+    requires junit;
 
 
     opens com.order.navigationdesign to javafx.fxml;
